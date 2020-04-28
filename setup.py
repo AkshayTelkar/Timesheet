e7f1e86b40a30dbe3a5a45cbb544dd74c405b807
Akshay Telkar <akshaytelkar@gmail.com>
2020-04-28 13:09:00 +0530

    Added setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Timesheet',
+    version='0.1',
+    packages=[''],
+    url='https://github.com/AkshayTelkar/Timesheet.git',
+    license='',
+    author='Akshay Telkar',
+    author_email='akshaytelkar@gmail.com',
+    description='Script to automate timesheet email'
+)
